94e118631740cecea80a31897dbf23aa4a2dc537
gustavlundgren <gustavlundgren23@gmail.com>
2024-10-20 09:14:52 +0200

    Get period function

--- script.js
+++ script.js
@@ -1,11 +1,43 @@
 //Todo, add dates for after this, have it be paused during permis
 //Toggle different cope levels so it isnt linear
-const permisDates = [[ new Date("October 24, 2024 14:10:00").getTime(), ],[ new Date("October 24, 2024 14:10:00").getTime()]]
-
+// const permisDates = [[new Date("October 24, 2024 14:10:00").getTime(),], [new Date("October 24, 2024 14:10:00").getTime()]]
 
 //Set the date we're counting down to
 const countdownDate = new Date("October 24, 2024 14:10:00").getTime();
 let cope = 0;
+
+// Function to choose what period you're in
+async function getDateByPeriod() {
+    let permisData = [];
+
+    await fetch('data.json')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+
+            permisData = data;
+        })
+        .catch(error => console.error('Error:', error));
+
+    const currentDate = new Date();
+
+
+    for (i in permisData) {
+
+        let p = permisData[i];
+
+
+        const startDate = new Date(p.duration.start);
+        const endDate = new Date(p.duration.end)
+
+
+        if (currentDate >= startDate && currentDate <= endDate) {
+
+            console.log({ endDate: p.duration.end, periodNumber: p.period });
+            return { endDate: p.duration.end, periodNumber: p.period };
+        }
+    }
+}
 
 
 //Function to update the countdown timer
@@ -17,41 +49,41 @@ function updateCountdown() {
 
 
     //FUuck switch
-    if (cope == 0){
+    if (cope == 0) {
         temp = countSecondsToTarget([[]]);
-    }else if (cope == 1){
-        temp = countSecondsToTarget([[22,0,5,40]]);
-    }else if (cope == 2){
-        temp = countSecondsToTarget([[22,0,5,40], [11,0,11, 45 ], [16,0,16, 45]]);
-    }else if (cope == 3){
-        temp = countSecondsToTarget([[22,0,5,40], [11,0,11, 45 ], [16,0,16, 45], [6,0,6,30]]);
-    }else if (cope == 4){
-        temp = countSecondsToTarget([[22,0,5,40], [11,0,11, 45 ], [16,0,16, 45], [6,0,6,30], [19,30,22,0]]);
-    }else if (cope == 5){
-        temp = countSecondsToTarget([[22,0,5,40], [11,0,11, 45 ], [16,0,16, 45], [6,0,6,30], [19,30,22,0]]);
+    } else if (cope == 1) {
+        temp = countSecondsToTarget([[22, 0, 5, 40]]);
+    } else if (cope == 2) {
+        temp = countSecondsToTarget([[22, 0, 5, 40], [11, 0, 11, 45], [16, 0, 16, 45]]);
+    } else if (cope == 3) {
+        temp = countSecondsToTarget([[22, 0, 5, 40], [11, 0, 11, 45], [16, 0, 16, 45], [6, 0, 6, 30]]);
+    } else if (cope == 4) {
+        temp = countSecondsToTarget([[22, 0, 5, 40], [11, 0, 11, 45], [16, 0, 16, 45], [6, 0, 6, 30], [19, 30, 22, 0]]);
+    } else if (cope == 5) {
+        temp = countSecondsToTarget([[22, 0, 5, 40], [11, 0, 11, 45], [16, 0, 16, 45], [6, 0, 6, 30], [19, 30, 22, 0]]);
         temp -= subtractSaturday();
-    }else if (cope == 6){
-        temp = countSecondsToTarget([[22,0,5,40], [11,0,11, 45 ], [16,0,16, 45], [6,0,6,30], [19,30,22,0], [5,40,6,0],  [6,30,8,0]]);
+    } else if (cope == 6) {
+        temp = countSecondsToTarget([[22, 0, 5, 40], [11, 0, 11, 45], [16, 0, 16, 45], [6, 0, 6, 30], [19, 30, 22, 0], [5, 40, 6, 0], [6, 30, 8, 0]]);
         temp -= subtractSaturday();
-    }else if (cope == 7){
-        temp = countSecondsToTarget([[22,0,5,40], [11,0,11, 45 ], [16,0,16, 45], [6,0,6,30], [19,30,22,0], [5,40,6,0],  [6,30,8,0], [8,0,11,0]]);
+    } else if (cope == 7) {
+        temp = countSecondsToTarget([[22, 0, 5, 40], [11, 0, 11, 45], [16, 0, 16, 45], [6, 0, 6, 30], [19, 30, 22, 0], [5, 40, 6, 0], [6, 30, 8, 0], [8, 0, 11, 0]]);
         temp -= subtractSaturday();
-    }else if (cope >= 8){
-        temp = countSecondsToTarget([[22,0,5,40], [11,0,11, 45 ], [16,0,16, 45], [6,0,6,30], [19,30,22,0], [5,40,6,0],  [6,30,8,0], [8,0,11,0]]);
+    } else if (cope >= 8) {
+        temp = countSecondsToTarget([[22, 0, 5, 40], [11, 0, 11, 45], [16, 0, 16, 45], [6, 0, 6, 30], [19, 30, 22, 0], [5, 40, 6, 0], [6, 30, 8, 0], [8, 0, 11, 0]]);
         temp -= subtractSaturday();
         koffein = true;
     }
-    
-    if (koffein){
-        temp = temp/2;
+
+    if (koffein) {
+        temp = temp / 2;
     }
     const distance = temp;
-    
-    const days    = Math.floor( distance / (60 * 60 * 24));
-    const hours   = Math.floor((distance % (60 * 60 * 24)) / (60 * 60)) + days * 24;
-    const minutes = Math.floor((distance % (60 * 60)) / ( 60));
+
+    const days = Math.floor(distance / (60 * 60 * 24));
+    const hours = Math.floor((distance % (60 * 60 * 24)) / (60 * 60)) + days * 24;
+    const minutes = Math.floor((distance % (60 * 60)) / (60));
     const seconds = Math.floor((distance % (60)));
-    
+
     // Create the formatted string for display
     let timeString = `${hours} timmar, ${minutes} minuter, och ${seconds} sekunder`;
 
@@ -68,7 +100,7 @@ setInterval(updateCountdown, 1000);
 function countSecondsToTarget(times) {
     const now = new Date(); // Current time
 
-    let targetDate = new Date(now.getFullYear(), 9, 24, 14, 0, 0); 
+    let targetDate = new Date(now.getFullYear(), 9, 24, 14, 0, 0);
     // Month is 0-based (October = 9)
 
     //If today is past cctober 24th, adjust to next year
@@ -76,10 +108,10 @@ function countSecondsToTarget(times) {
         targetDate.setFullYear(now.getFullYear() + 1);
     }
 
-    let totalSeconds = Math.trunc((targetDate - now)/1000); 
+    let totalSeconds = Math.trunc((targetDate - now) / 1000);
     //Total seconds until the target
-    
-    if (times.length > 0) {       
+
+    if (times.length > 0) {
         //Subtract sleeping seconds from the total
         for (let time of times) {
             if (time.length > 0) {
@@ -106,7 +138,7 @@ function calculateSleepingSeconds(aHour, aMinute, bHour, bMinute, endDate) {
     }
 
     let sleepingSeconds = 0;
-    const totalDays = Math.floor((endDate - now) / (1000 * 60 * 60 * 24)); 
+    const totalDays = Math.floor((endDate - now) / (1000 * 60 * 60 * 24));
     //Total days until target date
 
 
@@ -119,14 +151,14 @@ function calculateSleepingSeconds(aHour, aMinute, bHour, bMinute, endDate) {
         sleepingSeconds += (sleepEnd - now) / 1000; //We're in the sleep period today
     }
 
-    
+
 
     //Add sleep time for all the full days between now and the end date
     const sleepDurationPerDay = (sleepEnd - sleepStart) / 1000; //Duration of sleep in seconds
-    sleepingSeconds += (totalDays-1) * sleepDurationPerDay;
+    sleepingSeconds += (totalDays - 1) * sleepDurationPerDay;
 
-    
-    
+
+
     //Add sleep time for the last partial day
     let sleepStartNextDay = new Date(sleepStart);
     sleepStartNextDay.setDate(sleepStartNextDay.getDate() + totalDays);
@@ -146,47 +178,47 @@ function calculateSleepingSeconds(aHour, aMinute, bHour, bMinute, endDate) {
     return sleepingSeconds;
 }
 
-function subtractSaturday(){
+function subtractSaturday() {
     const now = new Date();
-    endDate   = new Date(now.getFullYear(), 9, 24, 14, 0, 0);
-    
-    
+    endDate = new Date(now.getFullYear(), 9, 24, 14, 0, 0);
+
+
     //Ensure that the end date is after the current date
     if (endDate <= now) {
-        return 0; 
+        return 0;
         //No time left to find a Saturday
     }
 
     //Loop through each day from now until the end date
     let currentDate = new Date(now); // Start from 'now'
-    
+
     while (currentDate <= endDate) {
         //Check if the current day is a Saturday 
         if (currentDate.getDay() === 6) {
             if (now.getDay() !== 6) {
-                return  9900
+                return 9900
             }
             const start = new Date(now);
             start.setHours(16, 45, 0, 0);
-            
 
-            if (now < start){
+
+            if (now < start) {
                 return 9900
             }
 
             const targetTime = new Date(now);
-            targetTime.setHours(19, 30, 0, 0); 
-        
+            targetTime.setHours(19, 30, 0, 0);
+
             //Calculate the difference between now and 1930
             const timeDiff = targetTime - now;
-        
+
             if (timeDiff <= 0) {
                 return 0;
             }
-        
-            
-        
-            return timeDiff/1000
+
+
+
+            return timeDiff / 1000
         }
 
         currentDate.setDate(currentDate.getDate() + 1);
@@ -219,7 +251,7 @@ function createFloatingText(text) {
 
     //Remove the text div after the animation is finished
     textDiv.addEventListener("animationend", () => {
-        textContainer.removeChild(textDiv); 
+        textContainer.removeChild(textDiv);
     });
 }
 
@@ -233,7 +265,7 @@ copeButton.addEventListener("click", () => {
     if (cope < strings.length) {
         const currentString = strings[cope];
         createFloatingText(currentString);
-        cope++; 
+        cope++;
         //COPE INCREASES
     } else {
         // Once all strings have been displayed, disable the button
